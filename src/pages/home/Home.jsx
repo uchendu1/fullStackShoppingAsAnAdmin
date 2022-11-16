cefa5825842eae6c7084e37bff5eec019e2a62b0
linda <lindatochukwu30@gmail.com>
2022-11-16 11:31:54 +0100

    comsuming api for userStatus but failed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect , useMemo} from "react";
+import { useState, useEffect, useMemo } from "react";
 import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import Layout from "../../components/layout";
@@ -6,43 +6,55 @@ import "./home.css";
 import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
+import {userRequest, BASE_URL } from "../../requestMethods";
+
 
 export default function Home() {
   const [userStats, setUserStats] = useState([]);
 
-  const MONTHS = useMemo(() => [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ],
-  []
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
   );
 
-  useEffect(()=> {
-const getStats = async ()=>{
-  try{
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get(BASE_URL + "/users/status");
+        res.data.pa((item) =>
+        setUserStats((prev) => [
 
-  }catch{}
-}
-  }, [])
-  
+          ...prev,
+          {name:MONTHS[item._id -1], "Active User": item.total},
+        ])
+        );
+      } catch {}
+    };
+    getStats()
+  }, [MONTHS]);
 
-
+  console.log(userStats, "userStats........")
 
   return (
     <Layout>
       <div className="home">
         <FeaturedInfo />
         <Chart
+    // data={userStats}
+
           data={userData}
           title="User Analytics"
           grid

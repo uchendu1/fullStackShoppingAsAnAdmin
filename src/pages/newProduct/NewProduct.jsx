a0651616d8f84cef9bce774fc1e4b329966e91d0
linda <lindatochukwu30@gmail.com>
2022-12-01 10:07:17 +0100

    adding usestate and onchange for the inputs

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -1,7 +1,14 @@
+import {useState} from "react";
 import "./newProduct.css";
 import Layout from "../../components/layout";
 
 export default function NewProduct() {
+
+  const [input,onChange={handleChange}   = useState({});
+  const [file, setFile] = useState(null);
+  const [input,onChange={handleChange}  = useState([]);
+
+
   return (
     <Layout>
       <div className="newProduct">
@@ -9,26 +16,31 @@ export default function NewProduct() {
         <form className="addProductForm">
           <div className="addProductItem">
             <label>Image</label>
-            <input type="file" id="file" />
+            <input onChange={e=> setFile(e.target.files)[0]} type="file" id="file" />
           </div>
           <div className="addProductItem">
             <label>Title</label>
-            <input type="text" placeholder="Apple Airpods" />
+            <input onChange={handleChange} type="text" placeholder="Apple Airpods" />
           </div>
           <div className="addProductItem">
             <label>Description</label>
-            <input type="text" placeholder="description........." />
+            <input onChange={handleChange} type="text" placeholder="description........." />
           </div>
 
           <div className="addProductItem">
             <label>Price</label>
-            <input type="text" placeholder="100" />
+            <input onChange={handleChange} type="text" placeholder="100" />
+          </div>
+
+          <div className="addProductItem">
+            <label>Categories</label>
+            <input onChange={handleCategories} type="text" placeholder="jeans, skirts, ......" />
           </div>
 
           <div className="addProductItem">
             <label>Stock</label>
 
-            <select>
+            <select  onChange={handleChange}>
               <option value="true">Yes</option>
               <option value="false">No</option>
             </select>
